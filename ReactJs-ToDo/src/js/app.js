38f83e654d72bd84911dafcb7e12749171c2301f
Rajasekhar <rajasekharb@live.com>
2016-09-05 19:31:09 +0530

    Added key to li and checked attribute

--- ReactJs-ToDo/src/js/app.js
+++ ReactJs-ToDo/src/js/app.js
@@ -1,7 +1,8 @@
+
 var TodoList = React.createClass({
     render: function() {
         var createItem = function(item) {
-            return <li>{item.text}</li>;
+            return (<li key={item.id}>{item.text}</li>);
         };
         return <ul>{this.props.items.map(createItem)}</ul>;
     }
@@ -13,23 +14,29 @@ var TodoApp = React.createClass({
     },
     
     onChange: function(e) {
-        this.setState({text: e.target.value});
+        var todo = e.target.value;
+        if (todo) {
+            this.setState({text: e.target.value});
+        }
     },
     
     handleSubmit: function(e) {
         e.preventDefault();
-        var updatedList = this.state.items.concat([{text: this.state.text, id: Date.now()}]);
-        this.setState({items: updatedList, text: ''});
+        var todo = this.state.text;
+        if (todo) {
+            var updatedList = this.state.items.concat([{text: todo, id: Date.now(), checked: false}]);
+            this.setState({items: updatedList, text: ''});
+        }
     },
     
     render: function() {
         return (
             <div>
-                <h3>Todo Application</h3>
+                <h3>ToDo</h3>
                 <TodoList items={this.state.items} />
                 <form onSubmit={this.handleSubmit}>
                     <input onChange={this.onChange} value={this.state.text}/>
-                    <button>{'Add #' + (this.state.items.length + 1)}</button>
+                    <button>{'Add #' + (this.state.items.length + 1) }</button>
                 </form>                     
             </div>
         );
